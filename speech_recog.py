f9a729e151145a2852fb11889a00f52519a1469d
Andrew212N <nguyen.andrew1234@yahoo.com>
2023-10-07 13:38:11 -0500

    added speech recog function

---
+++ speech_recog.py
@@ -0,0 +1,29 @@
+import speech_recognition as sr
+from typing import Dict, List
+
+r = sr.Recognizer()
+mic = sr.Microphone()
+
+def listen_to_mic(r: sr.Recognizer, mic: sr.Microphone) -> Dict:
+    with sr.Microphone() as source:
+        print("say something")
+        r.adjust_for_ambient_noise(source, duration = 0.5)
+        audio = r.listen(source)
+
+    result = {"success": True, "error": None, "transcript": None}
+
+    try:
+        #using google speec recog to translate voice to text
+        result["transcript"] = r.recognize_google(audio).lower()
+        #clean up audio
+        response["transcript"] = response["transcription"].replace("-", " ")
+        response["transcript"] = response["transcription"].replace("/", " ")
+        response["transcript"] = response["transcription"].replace("\\", " ")
+    
+    except sr.RequestError:
+        result["success"] = False
+        result["error"] = "Error with voice recognition"
+    except sr.UnknownValueError:
+        response["error"] = "Response not recognized"
+
+    return response
