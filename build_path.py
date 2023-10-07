59ff56e37dd07171d843f8b2741d3c09d07bce06
Andrew212N <nguyen.andrew1234@yahoo.com>
2023-10-07 17:53:40 -0500

    added comments

--- build_path.py
+++ build_path.py
@@ -18,17 +18,18 @@ build_path_redemption_mikael = [("Forbidden Idol", 800),
 def purchase_item(item):
     print(f'Processing {item}')
 
+#recursively goes through list and invokes purchase_item if current gold is greather than item cost
 def buy_items(items, current_gold):
     if not items:
+        #if item list is empty, end the recursion
         return
+    #iterates through item list
     for item in items.copy():
+        #if current gold is greather than item cost, then buy it
         if item[0] < current_gold:
             purchase_item(item)
             items.remove(item)
+        #if not, then if item has a sublist for components, then it goes through that list instead
         elif isinstance(item, list):
             buy_items(item, current_gold)
-
-    if len(items) == len(new_items):
-        new_items = [(str(i), i) for i in range(x, x+5)]
-        buy_items(new_items, x)
         
